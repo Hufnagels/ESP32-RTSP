3b62b3cf9d2560022e379ebfa34d90b361eea9f6
Hufnagels <kbvconsulting@gmail.com>
2022-07-23 21:19:06 +0200

    cors try

--- server/index.js
+++ server/index.js
@@ -26,9 +26,18 @@ const serverStart = async () => {
 
   app.use(cors())
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  })
 
   app.use('/', homeRoutes)
   app.use('/camera', feedRoutes)
+
+
   const server = createServer(app)
   const { proxy } = rtspRelay(server, app)
   console.log('proxy: ', proxy)
